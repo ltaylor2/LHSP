04a878a9b38890f506fcff8b0835d49202068743
Liam Taylor <ltaylor.ulysses@gmail.com>
2016-01-13 23:42:43 -0500

    Update R Visualization

--- src/lhsp.r
+++ src/lhsp.r
@@ -1,5 +1,4 @@
 # setwd("/DIR OF LHSP SRC/")
-
 setwd("LHSP/src")
 system("make clean")
 system("make")
@@ -7,14 +6,22 @@ system("./lhsp")
 
 results.df <- read.csv("Results.txt", header = FALSE)
 colnames(results.df) = c("Male_PC", "Male_RC", "Female_PC", "Female_RC", "Breeding_Success")
-
-# install.packages("ggplot2")
+# install.packages(c("ggplot2", "dplyr"))
 library(ggplot2)
+library(dplyr)
 
-ggplot(results.df, aes(x = Male_PC, y = Male_RC)) +
-  geom_tile(aes(fill = Breeding_Success))
-  
+maleStats <- results.df %>%
+                group_by(Male_PC, Male_RC) %>%
+                summarise(Percent_Success = mean(Breeding_Success) * 100)
 
-ggplot(results.df, aes(x = Female_PC, y = Female_RC)) +
-  geom_tile(aes(fill = Breeding_Success))
+femaleStats <- results.df %>%
+                group_by(Female_PC, Female_RC) %>%
+                summarise(Percent_Success = mean(Breeding_Success) * 100)
 
+ggplot(maleStats, aes(x = Male_PC, y = Male_RC)) +
+  geom_tile(aes(fill = Percent_Success)) +
+  scale_x_continuous(breaks = seq(0, 1, .25), labels = seq(0, 1, .25))
+
+ggplot(femaleStats, aes(x = Female_PC, y = Female_RC)) +
+  geom_tile(aes(fill = Percent_Success)) +
+  scale_x_continuous(breaks = seq(0, 1, .25), labels = seq(0, 1, .25))
