58242525ef673b8e18a10ddf6167a77a731aee89
unknown <ltaylor.ulysses@gmail.com>
2019-02-12 01:31:43 -0500

    Working on Life History proj

--- Scripts/Old_src/Egg.cpp
+++ Scripts/Old_src/Egg.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iostream>
 
 #include "Egg.h"
 
@@ -11,14 +12,17 @@ Egg::Egg():
 
 void Egg::eggDay(bool incubated)
 {
-    if (incubated)
+    if (incubated) {
+        std::cout << "I'm an egg! I'm being incubated" << std::endl;
         negCounter = 0;    
+    }
     else {
         negCounter++;
         if (negCounter > maxNegCounter)
             maxNegCounter = negCounter;
-        
         hatchDays += NEGLECT_PENALTY;
+        std::cout << "I'm an egg! I'm being neglected for " << negCounter << " days with the longest streak of "
+                  << maxNegCounter << " days. My new hatch days is " << hatchDays << std::endl;
     }
     
     // if the egg has been incubated to catch up to the number of days required to hatch
@@ -30,8 +34,10 @@ void Egg::eggDay(bool incubated)
     
     // if the egg has been incubating for a very long time building
     // up neglect days, it's dead in any case
-    if (currDays >= HATCH_DAYS_MAX)
+    if (currDays >= HATCH_DAYS_MAX) {
+        std::cout << "I am an egg! I just died from incubation lasting too long" << std::endl;
         alive = false;
+    }
     
     // next day!
     currDays++;
@@ -50,9 +56,12 @@ bool Egg::eggSurvival()
     // assumes random is seeded (do it in main()!)
     double chance = static_cast<double>(rand()) / RAND_MAX;
     
-    if (chance <= mortality)
+    if (chance <= mortality) {
+        std::cout << "I am an egg! I just died at hatch" << std::endl;
         return false;   // egg dies!
+    }
     
+    std::cout << "I am an egg! I just hatched succesfully" << std::endl;
     return true;    // egg survives!
 }
 
