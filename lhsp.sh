32585eae66ad55a9c83f463905b3ad99afa42f20
ltaylor2 <ltaylor.ulysses@gmail.com>
2026-05-11 01:53:28 -0400

    memory for hpc

--- lhsp.sh
+++ lhsp.sh
@@ -2,12 +2,13 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH -N 1
 #SBATCH -n 15
+#SBATCH --mem 200G
 
 cd /home/l.taylor/Documents/LHSP/src
 make clean
 make
 ./lhsp
 
-cd ..
+cd /home/l.taylor/Documents/LHSP
 Rscript --slave R/process_simulation_results.r
 Rscript --slave R/analysis.r
